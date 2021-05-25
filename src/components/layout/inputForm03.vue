4e485756ca7bd20c38c12f28edf874ac23b61635
autchadaporn <inging.510801@gmail.com>
2021-05-25 23:45:44 +0700

    Ex3

---
+++ src/components/layout/inputForm03.vue
@@ -0,0 +1,59 @@
+<template>
+  <div id="inputForm03">
+    <div class="container">
+      <div>{{title}}</div>
+      <div class="form-group">
+        <label for="userName">Name:</label>
+        <input
+          v-model="userName"
+          type="text"
+          class="form-control"
+          id="userName"
+          name="userName"
+          value= userName
+        />
+      </div>
+      <div class="form-group">
+        <label for="userAge">Age:</label>
+        <input
+          v-model="userAge"
+          type="text"
+          class="form-control"
+          id="userAge"
+          name="userAge"
+          value= userAge
+        />
+      </div>
+      <div class="form-group">
+        <label for="imageUrl">Image Url:</label>
+        <input
+          v-model="adress"
+          type="text"
+          class="form-control"
+          id="imageUrl"
+          name="imageUrl"
+        />
+      </div>
+      <div id="userComponent" class="my-4 py-3" style="background: rgb(238, 238, 238);">
+        <h2>User Component</h2>
+        <label>Name: {{userName}} </label>
+        <br>
+        <label>Age: {{userAge}}</label>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'InputForm03',
+  props: [
+    'title'
+  ],
+  data () {
+    return {
+      userName: '',
+      userAge: ''
+    }
+  }
+}
+</script>
--- src/views/Exercise_03.vue
+++ src/views/Exercise_03.vue
@@ -1,42 +1,14 @@
 <template>
-  <div id="exercise_02">
-    <h2>Exercise 3 (Photos|Components,Props)</h2>
-    <div class="container">
-      <div class="form-group">
-        <label for="name">Name:</label>
-        <input
-          v-model="name"
-          type="text"
-          class="form-control"
-          id="name"
-          name="name"
-        />
-      </div>
-      <div class="form-group">
-        <label for="age">Age:</label>
-        <input
-          v-model="phone"
-          type="text"
-          class="form-control"
-          id="age"
-          name="age"
-        />
-      </div>
-      <div class="form-group">
-        <label for="imageUrl">Image Url:</label>
-        <input
-          v-model="adress"
-          type="text"
-          class="form-control"
-          id="imageUrl"
-          name="imageUrl"
-        />
-      </div>
-    </div>
+  <div id="exercise_03">
+    <InputForm03 title="Exercise 3 (Photos|Components,Props)" />
   </div>
 </template>
 <script>
+import InputForm03 from '../components/layout/inputForm03'
 export default {
-  name: 'exercise_03'
+  name: 'exercise_03',
+  components: {
+    InputForm03
+  }
 }
 </script>
